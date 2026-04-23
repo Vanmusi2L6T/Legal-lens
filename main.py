be9292802f4156c816ef27845f560e5f1e6ad9c0
vansh-virmani <vanshvirmani319@gmail.com>
2026-04-24 02:13:50 +0530

    Added CORSMIDDLEWARE

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 # create app
 app = FastAPI()
+
+# Enable CORS for the frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ensure data folder exists
 os.makedirs("data/indexes", exist_ok=True)
